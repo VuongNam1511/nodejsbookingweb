954968a4f63fcd827f8545bb7f3e456a93aeee38
Joseph Vuong <vuongvannam1511.work@gmail.com>
2024-06-04 17:53:47 +0700

    before install cor

---
+++ src/controllers/userController.js
@@ -0,0 +1,30 @@
+
+import userService from "../services/userService";
+
+let HandleLogin = async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing input parameter!'
+        })
+    }
+
+    let userData = await userService.handleUserLogin(email, password);
+    console.log(userData)
+    // check email exist
+    // compare password
+    // return userInfor
+    // access_token: JWT Json web token
+    return res.status(200).json({
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        user: userData.user ? userData.user : {}
+    })
+}
+
+module.exports = {
+    HandleLogin: HandleLogin
+}
