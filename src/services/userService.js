a402a9ce50baf788614928791fdae0d1d99c8ae4
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-10-12 23:55:44 +0700

    #41

--- src/services/userService.js
+++ src/services/userService.js
@@ -120,24 +120,26 @@ let createNewUser = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your Email already used, please try another Email'
+                    errMessage: 'Your Email already used, please try another Email'
                 })
+            } else {
+                let hashPassWordFromBcrypt = await hashUserPassword(data.password)
+                await db.User.create({
+                    email: data.email,
+                    password: hashPassWordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId,
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                })
+
             }
-            let hashPassWordFromBcrypt = await hashUserPassword(data.password)
-            await db.User.create({
-                email: data.email,
-                password: hashPassWordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            });
         } catch (e) {
             reject(e);
         }
