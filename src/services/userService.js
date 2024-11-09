b258828c00fc977fc383317f25a3e6d8ddb1843c
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-11-09 17:40:05 +0700

    #62 WRITE AND READ FILE IMAGE with Sequelize and MySQL BLOB

--- src/services/userService.js
+++ src/services/userService.js
@@ -135,6 +135,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
+                    image: data.avatar,
                 })
                 resolve({
                     errCode: 0,
@@ -193,6 +194,9 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phonenumber = data.phonenumber
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save();
 
