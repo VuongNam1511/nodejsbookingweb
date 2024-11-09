4faa66fd11cc135795ef0bae7c9fd8b220576f4d
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-11-09 12:01:18 +0700

    #61 EDIT User wwith Redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -174,7 +174,7 @@ let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
             console.log('check nodejs', data)
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: ('Missing required parameter')
@@ -189,6 +189,10 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber
 
                 await user.save();
 
