7472f3a89a5d2f68e312a6e4468529598a5d3f70
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-11-17 20:57:08 +0700

    #68 Sequelize Relationship: Model & Database

--- src/route/web.js
+++ src/route/web.js
@@ -8,30 +8,23 @@ let initWebRouters = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
-
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayGetCRUD);
     router.get('/edit-crud', homeController.getEditCRUD);
-
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
-
-
-
 
     router.post('/api/login', userController.HandleLogin);
     router.get('/api/get-all-users', userController.HandleGetAllUsers);
     router.post('/api/create-new-user', userController.HandleCreateNewUser);
     router.put('/api/edit-user', userController.HandleEditUser);
     router.delete('/api/delete-user', userController.HandleDeleteUser); //restAPI
-
     router.get('/api/allcode', userController.getAllCode);
 
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
     router.get('/api/get-all-doctors', doctorController.getAllDoctors);
     router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
-
-
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
 
 
 
