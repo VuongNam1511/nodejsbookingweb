4116bfb1168c928e051b9de6863319d40c6a08f4
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-11-19 12:24:03 +0700

    #69 tao giao dien trang xem chi tiet thong tin bac si phia client

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -82,7 +82,7 @@ let getDetailDoctorById = (inputId) => {
             if (!inputId) {
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing required parameter'
+                    errMessage: 'Missing required parameter...'
                 })
             } else {
                 let data = await db.User.findOne({
@@ -90,7 +90,7 @@ let getDetailDoctorById = (inputId) => {
                         id: inputId
                     },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
                     include: [
                         {
@@ -101,9 +101,16 @@ let getDetailDoctorById = (inputId) => {
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
 
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+
+                }
+
+                if (!data) data = {};
 
                 resolve({
                     errCode: 0,
