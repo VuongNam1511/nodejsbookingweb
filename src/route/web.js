f5d3f544ad45c0d251870c7588a2122216ec9db6
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2025-01-22 23:30:24 +0700

    day96 tao du lieu cac co so y te/phong kham

--- src/route/web.js
+++ src/route/web.js
@@ -4,6 +4,8 @@ import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController"
 import specialtyController from "../controllers/specialtyController";
+import clinicController from "../controllers/clinicController";
+
 let router = express.Router();
 
 let initWebRouters = (app) => {
@@ -39,7 +41,9 @@ let initWebRouters = (app) => {
     router.get('/api/get-all-specialty', specialtyController.getAllSpecialty);
     router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
-
+    router.post('/api/create-new-clinic', clinicController.createClinic);
+    // router.get('/api/get-all-specialty', specialtyController.getAllSpecialty);
+    // router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
 
     return app.use("/", router);
